a52f40f102844940ac3447a625a3d73a4f95601a
Strelok_2012 <uraniumpuppy@gmail.com>
2023-11-15 20:17:16 +1100

    The basic functionality of writing a blog post
    In essence, it is a textarea that can be written to and which will then parse the contents of the textarea and turn them into a html form/page.

--- scripts/action.php
+++ scripts/action.php
@@ -1,4 +1,3 @@
 <?php
-echo "Hi" . htmlspecialchars($_POST['name']);
-echo "You are" . (int)$_POST['age'] . "years old.";
+
 ?>
--- scripts/blog_create.js
+++ scripts/blog_create.js
@@ -1,4 +1,18 @@
-function doThing()
+var add_line_btn = document.getElementById("blog_add_line");
+var blog_textarea = document.getElementById("blog_raw");
+
+console.log(document);
+
+console.log(blog_textarea);
+
+//function add_line_btn.onclick()
+//{
+//    print("Button pressed, adding line...");
+//    blog_textarea.value += "-----";
+//}
+
+function add_line_to_textarea()
 {
-    console.log("Doing a thing...");
+    console.log("Pressed da button, adding line (hopefully)");
+    blog_textarea.value += "-----";
 }

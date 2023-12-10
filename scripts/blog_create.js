344f8522d8c2b540e7b61835e3cafd99002ffb7f
Strelok_2012 <uraniumpuppy@gmail.com>
2023-12-10 18:31:25 +1100

    Minor change
    The precedent for major changes to the website that are to come. Specifically mobile compatibility.

--- scripts/blog_create.js
+++ scripts/blog_create.js
@@ -1,5 +1,6 @@
 var add_line_btn = document.getElementById("blog_add_line");
 var blog_textarea = document.getElementById("blog_raw");
+var blog_form = document.getElementById("blog_space");
 var textarea_selection;
 var textarea_selection_start;
 var textarea_selection_end;
@@ -35,8 +36,10 @@ function strengthen_text()
     {
         console.log("Text area selection was not empty");
         textarea_array.splice(textarea_selection_start, 0, "**");
-        textarea_array.splice(textarea_selection_end, 0, "**");
+        textarea_array.splice(textarea_selection_end + 1, 0, "**");
         blog_textarea.value = textarea_array.join("");
+        blog_textarea.selectionStart = textarea_selection_start + 2;
+        blog_textarea.selectionEnd = textarea_selection_end + 2;
     }
 
 }
@@ -63,6 +66,60 @@ document.addEventListener("click", get_text_area_selection);
     }
     console.log("Current text selection: " + text_area_selection_1);
 }*/
+
+function process_file_drop()
+{
+
+}
+
+function add_heading()
+{
+    const heading_3 = document.createElement("H1");
+    heading_3.innerText = "Heading 3";
+    heading_3.contentEditable = true;
+    blog_form.appendChild(heading_3);
+}
+
+function add_paragraph()
+{
+    paragraph = document.createElement("p");
+    paragraph.innerText = "paragraph";
+    paragraph.contentEditable = true;
+    blog_form.appendChild(paragraph);
+}
+
+function add_image()
+{
+    // Some sort of thing where I build a special image upload container??
+    console.log("Adding input element...");
+    // Create container
+    var img = document.createElement("DIV");
+    img.setAttribute("class", "img_upload");
+    //Create contents
+    var inputField = document.createElement("INPUT");
+    inputField.setAttribute("type", "file");
+    var submitButton = document.createElement("BUTTON");
+    submitButton.innerText = "Submit";
+    submitButton.setAttribute("type", "button");
+    submitButton.addEventListener("click", confirm_image(inputField.value));
+    img.appendChild(inputField);
+    img.appendChild(submitButton);
+    blog_form.appendChild(img);
+}
+
+function confirm_image(fileName)
+{
+    // Might be needed
+    var imgToCreate = document.createElement("IMG");
+    // Source is acquired how??
+    console.log("Value of input field: " + fileName);
+}
+
+function add_textarea()
+{
+    const textarea = document.createElement("textarea");
+    blog_form.appendChild(textarea);
+}
 
 function get_text_area_selection()
 {
